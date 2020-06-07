4cca466be9e4cc57202d3c9787512dfe78b79d0c
Vyctoria <vyctoriak@gmail.com>
2020-06-06 22:55:48 -0300

    erro de lógica nos selects

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -23,9 +23,13 @@ function getCities(event) {
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+  citySelect.innerHTML = ""
+  citySelect.disabled = true
+
   fetch(url)
   .then( res => res.json() )
   .then( cities => {
+    
     for( const city of cities ) {
       citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
     }
